cab1a3dc6929598b3503a874b12a69ce75ff1067
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-06-06 00:31:04 -0400

    added initial prompts

--- index.js
+++ index.js
@@ -7,3 +7,50 @@ db.connect((err) => {
   console.log("Database connected.");
   employee_tracker();
 });
+
+function employee_tracker() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "option",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employees role",
+          "Exit prompt?",
+        ],
+      },
+    ])
+    .then((response) => {
+      switch (response.option) {
+        case "View all departments":
+          selectAllDepartments();
+          break;
+        case "View all roles":
+          selectAllRoles();
+          break;
+        case "View all employees":
+          selectAllEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employees role":
+          updateEmployeeRole();
+          break;
+      }
+    });
+};
+
