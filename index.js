73d5fec49392e97b2a7c026158b5f045a8cee750
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-06-05 22:54:34 -0400

    error message

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 const table = require("console.table");
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  employee_tracker();
+});
