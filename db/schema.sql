b1eaa824782b33806542dd1c34cced819d3d2439
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-06-06 19:28:30 -0400

    changed database to solve issues

--- db/schema.sql
+++ db/schema.sql
@@ -2,24 +2,24 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
+
+   
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(8,2) NOT NULL,
-    department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    manager_id INT
 );
--- index.js
+++ index.js
@@ -1,50 +1,28 @@
 const inquirer = require("inquirer");
 const connection = require("./db/connection");
 
-db.connect((err) => {
+connection.connect((err) => {
   if (err) throw err;
   console.log("Database connected.");
   employeeTracker();
 });
 
 function employeeTracker() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "option",
-        message: "What would you like to do?",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employees role",
-          "Exit prompt.",
-        ]
-      }
-    ])
-    .then((response) => {
-                if (response.prompt === 'View all departments') {
-                    connection.query(`SELECT * FROM department`, (err, result) => {
-                        if (err) throw err;
-                        console.log("Currently viewing all departments");
-                        console.table(res);
-                        employeeTracker();
-                    });
-                } else if (response.prompt === 'View all roles') {
-                    connection.query(`SELECT * FROM role`, (err, result) => {
-                        if (err) throw err;
-                        console.log("Currently viewing all job roles");
-                        console.table(res);
-                        employeeTracker();
-                    });
-                } else if (response.prompt === 'View all employees') {
-                    connection.query(`SELECT * FROM employee`, (err, result) => {
-                        if (err) throw err;
-                        console.log("Currently viewing all employees in the database");
-                        console.table(res);
-                        employeeTracker();
-                    });
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "option",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employees role",
+        "Exit prompt.",
+      ],
+    },
+  ]);
+}
